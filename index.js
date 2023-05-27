58c61a04592396bf0e45c11f507718745767f82f
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-05-27 16:23:52 +0530

    made new changes and added max width min height

---
+++ index.js
@@ -0,0 +1,27 @@
+let cnv;
+
+window.onload = function(){
+    console.log("loading");
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+}
+
+window.onresize = function(){
+    console.log("resizing");
+    resizecanvas();
+}
+
+function resizecanvas(){
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    let veco = document.getElementById('vitor');
+// let ans = window.innerWidth-196;
+
+// veco.style.width = ans+"px";
+ 
+}
+
+
+    
+
+
